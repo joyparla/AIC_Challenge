ee4ad782f2f031e5244759a6cd55f7fad889e598
sparla <sparla@miracle.com>
2019-11-20 17:10:01 -0500

    Sol3

--- Solution3.java
+++ Solution3.java
@@ -18,7 +18,7 @@ public class Solution3 {
 
        String[]  strAr = input.split("@");
 
-       String[] strAr2 = strAr[1].split(".");
+       String[] strAr2 = strAr[1].split("\\.");
 
        if(!isAlpha(strAr[0]))
           return "Invalid";
@@ -27,18 +27,27 @@ public class Solution3 {
 
            if(i == strAr2.length -1){
 
-               //only alphabets
+        	   if(!isOnlyAlpha(strAr2[i]) || !(strAr2[i].length() == 2 || strAr2[i].length() == 3))
+        		   return "Invalid";
+        	   
            }else{
 
                if(!isAlpha(strAr2[i]))
                   return "Invalid";
            }
        }
+       
+       return "Valid";
 
     }
 
     private static boolean isAlpha(String em){
 
-         return strAr != null && strAr.matches("^[a-zA-Z0-9]*$");
+         return em != null && em.matches("^[a-zA-Z0-9]*$");
     }
+    
+    private static boolean isOnlyAlpha(String em){
+
+        return em != null && em.matches("^[a-zA-Z]*$");
+   }
 }
